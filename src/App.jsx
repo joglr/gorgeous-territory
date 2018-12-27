495aaadebeb4a870d888a249c7110002e0ad6151
Jonas Røssum <jonaswehoo@gmail.com>
2018-12-27 01:41:23 +0100

    added obstacles added collision prevention centered unitSized objects

--- src/App.jsx
+++ src/App.jsx
@@ -3,52 +3,98 @@ import { useSpring, useTransition, animated } from 'react-spring/hooks'
 import './App.css'
 import useKeys from './hooks/use-keys'
 
-const speed = 10
+const pixelUnit = 20
 const footprintOccurency = 2
 const historyLimit = 10
 const frameRate = 60
+const speed = 1 / frameRate
+const unitSize = {
+  width: CSS.px(pixelUnit),
+  height: CSS.px(pixelUnit)
+}
 
 function transformFromXY(pos) {
-  let x, y
-  if (pos.constructor === Array) [x, y] = pos
-  else [x, y] = Object.values(pos).map(x => x.value)
   return {
     position: 'absolute',
     left: 0,
     top: 0,
-    transform: `translate(${CSS.px(Math.floor(x))}, ${CSS.px(Math.floor(y))})`
+    transform: `translate(${pos
+      .map(v => v * pixelUnit)
+      .map(CSS.px)
+      .join(', ')})`
   }
 }
+
+const roundPixelPosToSquare = pixelPos => pixelPos.map(roundPixelsWithinUnit)
+const roundPixelsWithinUnit = pixels =>
+  Math.floor(pixels / pixelUnit) * pixelUnit
+const toUnits = pixels => Math.floor(pixels / pixelUnit)
+const toPixels = units => units * pixelUnit
+function generateObstacles({ width, height, amount }) {
+  // window size withing maximum amount of units
+  const size = [width, height]
+  return Array.from(new Array(amount), () =>
+    size.map(v => Math.random() * v).map(v => Math.floor(v))
+  )
+}
+
+const posEquals = ([x, y]) => ([otherX, otherY]) => otherX === x && otherY === y
+
+const obstacles = generateObstacles({
+  width: toUnits(window.innerWidth),
+  height: toUnits(window.innerHeight),
+  amount: 1000
+})
+
+const Obstacles = React.memo(function() {
+  return (
+    <>
+      {obstacles.map((pos, key) => (
+        <div
+          {...key}
+          style={{
+            ...unitSize,
+            ...transformFromXY(
+              // roundPosToSquare(
+              pos
+              // .map(toPixels)
+              // )
+            )
+          }}>
+          {'▨'}
+        </div>
+      ))}
+    </>
+  )
+})
 
 const App = function App() {
   const keys = useKeys(['up', 'down', 'left', 'right', 'space'])
   const [frameCount, setCount] = useState(0)
   const [history, setHistory] = useState([])
   const [pos, setPos] = useState([
-    Math.floor(window.innerWidth / 2),
-    Math.floor(window.innerHeight / 2)
+    toUnits(Math.floor(window.innerWidth / 2)),
+    toUnits(Math.floor(window.innerHeight / 2))
   ])
   function update() {
     setCount(frameCount + 1)
-    const [x, y] = pos
-    if (
-      !history.find(([otherX, otherY]) => otherX === x && otherY === y) &&
-      frameCount % footprintOccurency === 0
-    )
+    if (!history.find(posEquals(pos)) && frameCount % footprintOccurency === 0)
       setHistory([...history, pos])
     // else setHistory([])
     if (history.length > historyLimit) setHistory(history.slice(1))
     let diff = [0, 0]
 
-    if (keys.includes('left')) diff[0] = -speed
-    if (keys.includes('right')) diff[0] = +speed
-    if (keys.includes('up')) diff[1] = -speed
-    if (keys.includes('down')) diff[1] = +speed
+    if (keys.includes('left')) diff[0] = -speed * pixelUnit
+    if (keys.includes('right')) diff[0] = +speed * pixelUnit
+    if (keys.includes('up')) diff[1] = -speed * pixelUnit
+    if (keys.includes('down')) diff[1] = +speed * pixelUnit
     const nextPos = [pos[0] + diff[0], pos[1] + diff[1]]
-
-    if (diff[0] !== 0 || diff[1] !== 0) {
+    const intersectsWithObstacles = obstacles.some(
+      posEquals(roundPixelPosToSquare(nextPos.map(toPixels)).map(toUnits))
+    )
+    if ((diff[0] !== 0 || diff[1] !== 0) && !intersectsWithObstacles) {
       setPos(nextPos)
-      setDotProps(nextPos)
+      setDotProps(nextPos.map(Math.floor))
     }
   }
   useEffect(
@@ -78,10 +124,14 @@ const App = function App() {
     leave: { opacity: 0 }
   })
   return (
-    <div>
+    <div
+      style={{
+        fontSize: CSS.px(pixelUnit * 1.4),
+        lineHeight: CSS.px(pixelUnit * 1.4)
+      }}>
       <div>{JSON.stringify(keys)}</div>
       <div>{pos.join(', ')}</div>
-      {historyTransitions.map(({ item: [x, y], key, props }) => (
+      {/* {historyTransitions.map(({ item: [x, y], key, props }) => (
         <animated.div
           key={key}
           style={{
@@ -89,18 +139,17 @@ const App = function App() {
             left: 0,
             top: 0,
             transform: `translate(${CSS.px(x)}, ${CSS.px(y)})`,
-            fontSize: '30px',
             ...props
           }}>
           •
         </animated.div>
-      ))}
+      ))} */}
       <animated.div
         style={{
-          fontSize: '30px',
           textAlign: 'center',
           color: 'red',
-          ...transformFromXY(dotProps)
+          ...unitSize,
+          ...transformFromXY(Object.values(dotProps).map(({ value }) => value))
         }}>
         {arrowMap[
           keys
@@ -109,6 +158,7 @@ const App = function App() {
             .join('')
         ] || '•'}
       </animated.div>
+      <Obstacles />
     </div>
   )
 }
